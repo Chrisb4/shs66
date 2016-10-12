9fce12d9cc3ab82f940243b7324bf64185d1d325
Christopher Brown <nudgechris@gmail.com>
2016-10-12 15:27:03 -0500

    added pics-fixing grid

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,10 +7,11 @@ var modal = document.getElementById('myModal');
 var img = document.getElementById('myImg');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-var downloadButton = document.getElementById("downloadButton";)
+var downloadButton = document.getElementById("downloadButton");
+var newSource = "./images/thumbnails/tpic20.jpg";
 img.onclick = function(){
     modal.style.display = "block";
-    modalImg.src = this.src;
+    modalImg.src = newSource;
 }
 
 // Get the <span> element that closes the modal
