6163e68b1648b47906eaf93f3ce0afa64e24683f
Christopher Brown <nudgechris@gmail.com>
2016-10-13 00:00:33 -0500

    modals work, adding download link to higher res

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,27 @@
 $( document ).ready(function() {
-console.log('\'Allo \'Allo!');
 
-  // Get the modal
-  var modal = document.getElementById('myModal');
-
-  // Get the image and insert it inside the modal - use its "alt" text as a caption
-  var img = document.getElementById("myImg");
-  var modalImg = document.getElementById("img01");
-  var captionText = document.getElementById("caption");
-  img.onclick = function(){
+  // When any of the thumbnail pics are clicked, pass it into the function to get the id
+  $('.pic-thumbnail').click(function(e){
+    // get the id of the thumbnail that was clicked
+    var img = document.getElementById(e.target.id);
+    //target the img element in the modal div so it can be set when it is launched
+    var modalImg = document.getElementById("modalImage");
+    // Function to launch the modal.  It changes the display from none to block and inherits the image
+    //source to the image id that was clicked on
+    var modalLauncher = function() {
+      var modal = document.getElementById('myModal');
+      var download = document.getElementById('downloadButton');
       modal.style.display = "block";
-      modalImg.src = this.src;
-      console.log("clicked the id");
-  }
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
+      modalImg.src = img.src;
+      download.href = img.src;
+    }
+    modalLauncher();
+  });
+  // Closes the modal when the X is clicked
+  $('#closeSpan').click(function() {
+    var modal = document.getElementById('myModal');
     modal.style.display = "none";
-  }
+  });
 
+var what = "the fuck";
 });
